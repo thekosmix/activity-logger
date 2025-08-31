87cc1b8c1ba1dda80905d74d94d63d3d4694f2d7
Siddharth Kumar <siddharth.dce08@gmail.com>
2025-08-31 18:44:08 +0530

    adjusted routes

--- app/_layout.js
+++ app/_layout.js
@@ -1,32 +1,9 @@
-import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router';
 
-export default function AppLayout() {
+export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="home"
-        options={{
-          tabBarLabel: 'Home',
-        }}
-      />
-      <Tabs.Screen
-        name="add-activity"
-        options={{
-          tabBarLabel: 'Add Activity',
-        }}
-      />
-      <Tabs.Screen
-        name="menu"
-        options={{
-          tabBarLabel: 'Menu',
-        }}
-      />
-      <Tabs.Screen
-        name="admin/employees"
-        options={{
-          tabBarLabel: 'Employees',
-        }}
-      />
-    </Tabs>
+    <Stack>
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
+    </Stack>
   );
 }
